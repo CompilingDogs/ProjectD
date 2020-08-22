3d224ac18f09ff74811e1576f6f2217a2f297904
IamMaxim <g.maxim.stepanoff@gmail.com>
2020-08-22 16:30:21 +0300

    Added libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     kotlin("jvm") version "1.3.72"
+    id("io.freefair.lombok") version "5.1.1"
 }
 
 group = "com.compilingdogs"
@@ -13,4 +14,5 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     testCompile("junit", "junit", "4.12")
+    compile("org.apache.logging.log4j", "log4j-slf4j-impl", "2.13.3")
 }
