c2abf07cb3256fbf09ffe59d7843e76af9e680ff
IamMaxim <g.maxim.stepanoff@gmail.com>
2020-09-15 19:23:24 +0300

    Added jar build settings. Use as     ./gradlew installDist And find your files in ./build/install Execute like bin/ProjectD

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     java
     kotlin("jvm") version "1.4.0"
     kotlin("kapt") version "1.4.0"
+    application
+}
+
+application {
+    mainClassName = "Main"
 }
 
 group = "com.compilingdogs"
@@ -12,14 +17,10 @@ repositories {
     maven(url = "https://dl.bintray.com/arrow-kt/arrow-kt/")
 }
 
+
 dependencies {
     implementation(kotlin("stdlib", "1.4.0"))
     implementation("junit", "junit", "4.12")
     implementation("com.google.code.gson", "gson", "2.8.6")
     implementation("org.apache.logging.log4j", "log4j-slf4j-impl", "2.13.3")
-
-//    val arrowVersion = "0.10.4"
-//    implementation("io.arrow-kt:arrow-core:$arrowVersion")
-//    implementation("io.arrow-kt:arrow-syntax:$arrowVersion")
-//    kapt("io.arrow-kt:arrow-meta:$arrowVersion")
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,5 @@ public class Main {
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-
     }
 }
